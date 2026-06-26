d713520b82c1703eace671dacced9f625932e788
Bruno Herculano <162291567+Bruno-H-Terto@users.noreply.github.com>
2026-06-26 00:01:46 -0300

    test: replace custom scenarios with documented examples

--- tests/test_question_2.py
+++ tests/test_question_2.py
@@ -3,17 +3,11 @@ import pytest
 
 
 def test_must_return_a_term_position_of_arithmetic_sequence():
-    first_term = last_sequence_term(first_term=11, common_difference=7, term_position=1)
-    second_term = last_sequence_term(
-        first_term=11, common_difference=7, term_position=2
-    )
-    ninth_hundred_ninety_ninth_term = last_sequence_term(
-        first_term=11, common_difference=7, term_position=999
-    )
-
-    assert first_term == 11
-    assert second_term == 18
-    assert ninth_hundred_ninety_ninth_term == 6997
+    assert last_sequence_term(11, 7, 1) == 11
+    assert last_sequence_term(11, 7, 2) == 18
+    assert last_sequence_term(11, 7, 200) == 1404
+    assert last_sequence_term(11, 7, 254) == 1782
+    assert last_sequence_term(11, 7, 3_542_158) == 24_795_110
 
 
 def test_must_be_raise_if_term_position_is_lower_than_1():
